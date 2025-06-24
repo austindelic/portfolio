551f22fc2db8d61b47b3e5e18920043320489ed2
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-24 15:36:15 -0700

    I watched a tutorial and it sorta works but inline html doesn't I honestly don't think i have the skill to do what i want to do so i probably need to pivot

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,18 +1,18 @@
+import MarkdownRenderer from '@/app/components/MarkdownRenderer';
 import { fetch_project_by_slug } from '@/lib/db';
 import { fetchReadmeFromRepo } from '@/lib/github';
-import { mdToCleanHtml } from '@/lib/markdown';
 
-
+// src/app/components/markdownRenderer.tsx location of markdown renderer
+// src/app/posts/[slug]/page.tsx  current locatiom
 export default async function PostPage({params: {slug}}: {params: {slug: string}}) {
 	try {
 		const project = await fetch_project_by_slug(slug);
 		const url = project.url;
 		const readme = await fetchReadmeFromRepo(url); 
-		const { clean_html } = await mdToCleanHtml(readme);
 		console.log('Project:', project);
 		console.log('README:', readme);
 		return (
-			<div dangerouslySetInnerHTML={{ __html: clean_html || ''}} />
+			<MarkdownRenderer markdown={readme} />
 		);
 	}
 	catch (error) {
