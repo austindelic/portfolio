aa4c2a19b205c1917dedeee944476c061a7b2aec
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-25 14:04:28 -0700

    put it to the left, looking for an image to put below my name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 export default function Home() {
   return (
-    <div className="text-center pt-20">
+    <div className="pt-20">
       
-      <div className="mx-auto mt-8 w-3/4 max-w-xl h-64 border-2 border-black flex items-center">
-        <h1 className="text-3xl font-bold">hi, i'm [armand]</h1>
+      <div className="mt-8 w-3/4 max-w-xl h-64 border-2 border-black p-4">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-3xl font-bold">hi, i'm [armand]</h1>
+
+        </div>
+        
       </div>
     </div>
   );
