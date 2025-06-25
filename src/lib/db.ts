ce34b4815d5d6d54078b4c1e67e6c4a406e3d2bf
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-25 00:29:12 -0700

    fun little function for returning a projects technologies, currently it's not perfect but ill improve it

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,6 @@
 "use server"
 import { drizzle } from 'drizzle-orm/postgres-js';
+import { eq } from 'drizzle-orm';
 import postgres from 'postgres';
 import { projects, projectTechnologies, technologies } from '../db/schema';
 import { UUID } from 'crypto';
@@ -8,10 +9,30 @@ export async function fetchAllProjects(){
   const client = postgres(process.env.DATABASE_URL!);
   const db = drizzle(client);
   try {
-    const allLinks = await db.select().from(projects);
-    return allLinks;
+    const allProjectsWithTech = await db
+    .select({
+      project: projects,
+      technologies: technologies
+    })
+    .from(projects)
+    .leftJoin(projectTechnologies, eq(projectTechnologies.projectId, projects.id))
+    .leftJoin(technologies, eq(technologies.id, projectTechnologies.technologyId));
+
   } catch (error) {
     console.error(error);
     throw error;
   }
+}
+
+export async function fetchTechnologiesByProjectId(projectId: UUID) {
+  const client = postgres(process.env.DATABASE_URL!);
+  const db = drizzle(client);
+
+  const technologiesForProject = await db
+    .select()
+    .from(projectTechnologies)
+    .where(eq(projectTechnologies.projectId, projectId))
+    .leftJoin(technologies, eq(technologies.id, projectTechnologies.technologyId));
+  
+  return technologiesForProject;
 }
