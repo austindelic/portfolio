a09c04540b5e5eea91ddc100356db3d5890bcc48
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-24 21:02:58 -0700

    ugh idk why it's hanging forever

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,17 +3,16 @@ import { UUID } from "crypto";
 import ProjectCard from "../components/projectCard";
 
 export default async function page() {
-	const data = await fetch_example()
+	const data: { id: string; name: string; url: string; logoImage: string | null; headerImage: string | null; description: string }[] = await fetch_example();
 	
 	return (
 		<div className="text-center pt-20">
 			<h1 className="text-3xl font-bold">Posts</h1>
 			<ul className="flex flex-wrap justify-center gap-6 p-6">
-				{data.map((project: { id: string; name: string; url: string; slug: string; logoImage: string | null; headerImage: string | null; description: string }) => (
+				{data.map((project: { id: string; name: string; url: string; logoImage: string | null; headerImage: string | null; description: string }) => (
 					<li key={project.id}>
 						<ProjectCard 
 						title={project.name} 
-						slug={project.slug}
 						url={project.url}
 						logoImage={project.logoImage || undefined}
 						headerImage={project.headerImage || undefined}
