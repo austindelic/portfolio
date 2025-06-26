78eb356ae6905949c2e8be79c52d07ae14cce578
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-26 07:28:32 -0700

    fix no un-escaped error

--- src/app/components/greetingCard.tsx
+++ src/app/components/greetingCard.tsx
@@ -6,7 +6,7 @@ export default function GreetingCard() {
       <div className="mt-8 w-fit border-2 border-black p-4 mx-auto">
         <div className="flex flex-row gap-4">
           <div className="flex flex-col gap-2" style={{ width: `${textWidthPercent}%` }}>
-            <h1 className="text-3xl font-bold w-fit">hi, i'm [armand]</h1>
+            <h1 className="text-3xl font-bold w-fit">hi, i&#39;m [armand]</h1>
             <p className="font-semibold break-words w-full max-w-full text-justify">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
