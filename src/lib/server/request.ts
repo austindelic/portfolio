a626e2282531e9b28c5c27568cd4663d9c2e76fa
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-26 10:31:22 -0700

    erm maybe

--- src/lib/server/request.ts
+++ src/lib/server/request.ts
@@ -3,22 +3,19 @@ import { TokenBucket } from "./rate-limit";
 
 export const globalBucket = new TokenBucket<string>(100, 1);
 
-export function globalGETRateLimit(): boolean {
-	// Note: Assumes X-Forwarded-For will always be defined.
-	const clientIP = headers().get("X-Forwarded-For");
+
+export async function globalGETRateLimit(): Promise<boolean> {
+	const clientIP = (await headers()).get("X-Forwarded-For");
 	if (clientIP === null) {
 		return true;
 	}
 	return globalBucket.consume(clientIP, 1);
 }
 
-
-export function globalPOSTRateLimit(): boolean {
-	// Note: Assumes X-Forwarded-For will always be defined.
-	const clientIP = headers().get("X-Forwarded-For");
+export async function globalPOSTRateLimit(): Promise<boolean> {
+	const clientIP = (await headers()).get("X-Forwarded-For");
 	if (clientIP === null) {
 		return true;
 	}
 	return globalBucket.consume(clientIP, 3);
 }
-
