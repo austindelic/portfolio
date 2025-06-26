4b246aa9ccf3819454b65485e115069008357aa4
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-26 15:47:49 -0700

    not needed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
-import type { User as PrismaUser } from "@prisma/client";
 import GreetingCard from "./components/greetingCard";
 import ProjectsView from "./components/ProjectsView";
-import { getCurrentSession } from "@/lib/server/session";
-import { redirect } from "next/navigation";
-import { LogoutButton } from "./components";
 import { globalGETRateLimit } from "@/lib/server/request";
 
 export default async function Home() {
   if (!(await globalGETRateLimit())) {
     return "Too many requests";
   }
-  const { user } = await getCurrentSession();
-  const profile = user as PrismaUser; // gives typed access to username, email, githubId
-  if (user === null) {
-    return redirect("/login");
-  }
-  const image = `https://avatars.githubusercontent.com/u/${profile.githubId}`;
-
   return (
     <div className="flex flex-col items-center gap-12">
       <GreetingCard />
@@ -28,12 +17,6 @@ export default async function Home() {
           </a>
         </div>
         <ProjectsView limit={4} />
-      </div>
-      <div className="flex flex-col items-center gap-4">
-        <h1>Hi, ${profile.githubId}!</h1>
-        <img src={image} height="100px" width="100px" alt="profile" />
-        <p>Email: {profile.email}</p>
-        <LogoutButton />
       </div>
     </div>
   );
