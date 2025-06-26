5be5a37104a366b84e9c7e14ca975fcb72ae2ea3
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-26 15:58:14 -0700

    awesome, let's push this to vercel

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,25 @@
+import type { User as PrismaUser } from "@prisma/client";
+import { getCurrentSession } from "@/lib/server/session";
+import { redirect } from "next/navigation";
+import { LogoutButton } from "../components";
 
-export default function Page() {
+export default async function Page() {
+  const { user } = await getCurrentSession();
+  if (!user) {
+    redirect("/login");
+  }
+
+  const profile = user as PrismaUser;
+  const image = `https://avatars.githubusercontent.com/u/${profile.githubId}`;
+
   return (
-	<div className="flex flex-col items-center gap-12 pt-20">
-		<h1 className="text-3xl font-bold">[admin]</h1>
-	</div>
+    <div className="flex flex-col items-center gap-12 pt-20">
+      <h1 className="text-3xl font-bold">[admin]</h1>
+      <div className="flex flex-col items-center gap-4">
+        <img src={image} alt="profile" height="100" width="100" className="rounded-full" />
+        <p className="font-semibold">{profile.email}</p>
+        <LogoutButton />
+      </div>
+    </div>
   );
 }
--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,8 +1,14 @@
 import Link from 'next/link';
+import { getCurrentSession } from "@/lib/server/session";
 
-export default function Footer() {
+
+export default async function Footer() {
+	const { user } = await getCurrentSession();
+	const isLoggedIn = !!user;
+
+
 	return (
-		<footer className="h-auto py-4 px-6 text-black  font-[900] text-sm flex flex-col md:flex-row justify-between items-center gap-4">
+		<footer className="h-auto py-4 px-6 text-black font-[900] text-sm flex flex-col md:flex-row justify-between items-center gap-4">
 			<div className="flex items-center gap-2 font-medium">
 				<span>&copy; {new Date().getFullYear()} Armand Packham-McGuiness</span>
 			</div>
@@ -16,9 +22,11 @@ export default function Footer() {
 				<li>
 					<Link href="/contact" className="hover:underline font-medium">[contact]</Link>
 				</li>
-				<li>
-					<Link href="/admin" className="hover:underline font-medium">[admin]</Link>
-				</li>
+				{isLoggedIn !== null && (
+					<li>
+						<Link href={isLoggedIn ? "/admin" : "/login"} className="hover:underline font-medium">[admin]</Link>
+					</li>
+				)}
 			</ul>
 		</footer>
 	);
