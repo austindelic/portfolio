d7ac4959978881ecdee644741d85fd6e4aec4b8b
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-26 07:25:39 -0700

    make footer pretty

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,8 +1,25 @@
-import link from 'next/link';
+import Link from 'next/link';
 
 export default function Footer() {
 	return (
-		<div className="h-[40px] flex items-center justify-between ">
-		</div>
+		<footer className="h-auto py-4 px-6 text-black  font-[900] text-sm flex flex-col md:flex-row justify-between items-center gap-4">
+			<div className="flex items-center gap-2 font-medium">
+				<span>&copy; {new Date().getFullYear()} Armand Packham-McGuiness</span>
+			</div>
+			<ul className="flex flex-row items-center gap-6">
+				<li>
+					<Link href="/home" className="hover:underline font-medium">[home]</Link>
+				</li>
+				<li>
+					<Link href="/projects" className="hover:underline font-medium">[projects]</Link>
+				</li>
+				<li>
+					<Link href="/contact" className="hover:underline font-medium">[contact]</Link>
+				</li>
+				<li>
+					<Link href="/admin" className="hover:underline font-medium">[admin]</Link>
+				</li>
+			</ul>
+		</footer>
 	);
 }
